59a61602d134177271c2000e20458759aac045fa
root <root@raspberrypi.(none)>
2013-04-24 02:06:12 +0000

    j

--- inputrc.py
+++ inputrc.py
@@ -5,7 +5,7 @@ GPIO.setmode(GPIO.BOARD)
 pin = 26
 GPIO.setup(pin, GPIO.IN)
 
-sys.stdout = open('/dev/servoblaster', 'w')
+#sys.stdout = open('/dev/servoblaster', 'w')
 
 print '0=20'
 sys.stdout.flush()
@@ -21,23 +21,37 @@ def smoother(value, array, index):
 	array[index] = value
 	return sum(array)/len(array)
 
-bufferlength = 25
-array = [0] * bufferlength
+bufferlength = 3
+array = [120] * bufferlength
+
+min = 1000000
+max = -1000000
 
 while True:
 	try:
 		counter = 0
 		if (GPIO.input(pin)):
+			#This measures the width of the pulse
 			while(GPIO.input(pin)):
 				counter += 1
 				time.sleep(0.00000001)
-			angle = int(counter * 5.581 - 235)
 
-			index += 1
-			if index >= bufferlength: index = 0
-			angle = smoother(angle, array, index)
+#			#This executes the smoothing function
+#			index += 1
+#			if index >= bufferlength: index = 0
+#			angle = smoother(counter, array, index)
+#			
+#			#This automatically maps the range of the control to the range of the servo
+#			if angle<min: min=angle
+#			if angle>max: max=angle
+#			range=max-min
+#			if not range == 0:
+#				mult=250/range
+#				angle = int(mult*(angle - min))
 
-			print '0=' + str(angle)
+			#This turns the servo
+#			print angle
+			print counter
 			sys.stdout.flush()
 	except KeyboardInterrupt:
 		sys.exit()
